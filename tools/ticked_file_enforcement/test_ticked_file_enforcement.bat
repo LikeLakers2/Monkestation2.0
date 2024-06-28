6df5ae4defc23c9f4b21a9585ac1223df01a00ec
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-06-28 13:13:36 -0400

    Add some blank echo statements to make it easier to read the test output
    I plan to turn this to a python script later

--- tools/ticked_file_enforcement/test_ticked_file_enforcement.bat
+++ tools/ticked_file_enforcement/test_ticked_file_enforcement.bat
@@ -5,12 +5,18 @@ type tools\ticked_file_enforcement\tests\includes_file_should_have_includes.json
 echo.
 echo ---- WARNING TESTS ----
 type tools\ticked_file_enforcement\tests\begin_include_should_not_be_nested.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+echo.
 type tools\ticked_file_enforcement\tests\unincluded_glob_should_match_a_file.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+echo.
 type tools\ticked_file_enforcement\tests\unincluded_globs_should_not_match_included_file.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+echo.
 type tools\ticked_file_enforcement\tests\warn_if_end_include_without_begin_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 
 echo.
 echo ---- ERROR TESTS ----
 type tools\ticked_file_enforcement\tests\base_scanning_directory_must_be_valid.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+echo.
 type tools\ticked_file_enforcement\tests\includes_file_must_have_begin_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+echo.
 type tools\ticked_file_enforcement\tests\includes_file_must_have_end_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+echo.
