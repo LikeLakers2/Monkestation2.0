5407d57c419bba7b6f3185c2ed300a5a2fc6d82b
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-06-28 13:02:17 -0400

    Reorganize tests (and also commit the base scanning directory test)

--- tools/ticked_file_enforcement/test_ticked_file_enforcement.bat
+++ tools/ticked_file_enforcement/test_ticked_file_enforcement.bat
@@ -1,15 +1,15 @@
 @echo off
 echo ---- NOTICE TESTS ----
-type tools\ticked_file_enforcement\tests\notices\no_includes.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+type tools\ticked_file_enforcement\tests\includes_file_should_have_includes.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 
 echo.
 echo ---- WARNING TESTS ----
-type tools\ticked_file_enforcement\tests\warnings\end_include_without_begin_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
-type tools\ticked_file_enforcement\tests\warnings\nested_begin_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
-type tools\ticked_file_enforcement\tests\warnings\unincluded_glob_matches_included_file.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
-type tools\ticked_file_enforcement\tests\warnings\unincluded_glob_matches_no_files.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+type tools\ticked_file_enforcement\tests\begin_include_should_not_be_nested.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+type tools\ticked_file_enforcement\tests\unincluded_glob_should_match_a_file.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+type tools\ticked_file_enforcement\tests\warn_if_end_include_without_begin_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 
 echo.
 echo ---- ERROR TESTS ----
-type tools\ticked_file_enforcement\tests\errors\missing_begin_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
-type tools\ticked_file_enforcement\tests\errors\missing_end_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+type tools\ticked_file_enforcement\tests\base_scanning_directory_must_be_valid.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+type tools\ticked_file_enforcement\tests\includes_file_must_have_begin_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+type tools\ticked_file_enforcement\tests\includes_file_must_have_end_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
