f76c1c2bcd3487e25ad65e30ce61c092fb92e93e
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-06-28 15:10:10 -0400

    Rename end_include test to be more accurate to what it's testing

--- tools/ticked_file_enforcement/test_ticked_file_enforcement.bat
+++ tools/ticked_file_enforcement/test_ticked_file_enforcement.bat
@@ -6,11 +6,11 @@ echo.
 echo ---- WARNING TESTS ----
 type tools\ticked_file_enforcement\tests\begin_include_should_not_be_nested.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 echo.
+type tools\ticked_file_enforcement\tests\end_include_should_not_be_outside_include_area.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+echo.
 type tools\ticked_file_enforcement\tests\unincluded_glob_should_match_a_file.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 echo.
 type tools\ticked_file_enforcement\tests\unincluded_globs_should_not_match_included_file.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
-echo.
-type tools\ticked_file_enforcement\tests\warn_if_end_include_without_begin_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 
 echo.
 echo ---- ERROR TESTS ----
