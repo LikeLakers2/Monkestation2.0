43f615b439e22538cd0b041534046fe506911d41
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-06-30 10:30:27 -0400

    Distinguish between error tests that immediately exit, and those that don't immediately exit

--- tools/ticked_file_enforcement/test_ticked_file_enforcement.bat
+++ tools/ticked_file_enforcement/test_ticked_file_enforcement.bat
@@ -1,8 +1,8 @@
 @echo off
 echo ---- NOTICE TESTS ----
 echo No tests.
-
 echo.
+
 echo ---- WARNING TESTS ----
 type tools\ticked_file_enforcement\tests\begin_include_should_not_be_nested.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 echo.
@@ -19,13 +19,14 @@ echo.
 type tools\ticked_file_enforcement\tests\schema\forbidden_include_glob_should_match_a_file.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 echo.
 
-echo.
 echo ---- ERROR TESTS ----
+type tools\ticked_file_enforcement\tests\includes_file_must_not_include_a_file_multiple_times.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
+echo.
+
+echo ---- FATAL ERROR TESTS ----
 type tools\ticked_file_enforcement\tests\includes_file_must_have_begin_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 echo.
 type tools\ticked_file_enforcement\tests\includes_file_must_have_end_include.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
-echo.
-type tools\ticked_file_enforcement\tests\includes_file_must_not_include_a_file_multiple_times.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 echo.
 type tools\ticked_file_enforcement\tests\schema\base_scanning_directory_must_be_within_includes_file_directory.json | tools\bootstrap\python tools\ticked_file_enforcement\ticked_file_enforcement.py
 echo.
